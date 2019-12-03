3ba90792f7de3880e9b674299ccc4a9229d6eb3a
izio38 <romainbillot3009@gmail.com>
2019-12-03 16:41:03 +0100

    upload( models/mock )

---
+++ src/models/Etudiant.java
@@ -0,0 +1,43 @@
+package models;
+
+public class Etudiant {
+	
+	private Integer id;
+	private String prenom;
+	private String nom;
+	
+	public Etudiant() {
+		super();
+	}
+	
+	public Etudiant(Integer id, String prenom, String nom) {
+		super();
+		this.id = id;
+		this.prenom = prenom;
+		this.nom = nom;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+}
---
+++ src/models/GestionFactory.java
@@ -0,0 +1,66 @@
+package models;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Random;
+
+public class GestionFactory {
+
+	/////// SIMULATION DE LA PERSISTANCE DES ETUDIANTS ET DES ABSENCES
+	
+	// CHARGER en premier à l'execution du projet (car constante : static final)
+	private static final HashMap<Integer, Etudiant> LISTE_ID_ETUDIANTS = intializeListEtudiants();
+	private static final HashMap<Integer, Integer> LISTE_ID_ABSENCES = intializelistEtudiantAbsence();
+
+	// Initialisation des étudiants
+	private static HashMap<Integer, Etudiant> intializeListEtudiants() {
+
+		// Création des étudiants
+		Etudiant etu1 = new Etudiant(0, "Brunet-Manquat", "Francis");
+		Etudiant etu2 = new Etudiant(1, "Martin", "Philippe");
+
+		// Création du hasmap (association clé/valeur)
+		// Association id -> etudiant
+		HashMap<Integer, Etudiant> listEtudiantsTemp = new HashMap<>();
+		listEtudiantsTemp.put(etu1.getId(), etu1);
+		listEtudiantsTemp.put(etu2.getId(), etu2);
+
+		//
+		return listEtudiantsTemp;
+	}
+
+	// Initialisation des absences
+	private static final HashMap<Integer, Integer> intializelistEtudiantAbsence() {
+
+		// Création du hasmap (association clé/valeur)
+		// Association etudiant id -> absences
+		HashMap<Integer, Integer> listEtudiantAbsenceTemp = new HashMap<>();
+		
+		// Le nombre d'absences est généré aléatoirement
+		Random rand = new Random();
+		for (Etudiant etudiant : LISTE_ID_ETUDIANTS.values()) {
+			listEtudiantAbsenceTemp.put(etudiant.getId(), rand.nextInt(10));
+		}
+
+		//
+		return listEtudiantAbsenceTemp;
+	}
+
+	
+	/////// METHODES A UTILISER
+	// Retourne l'ensemble des etudiants
+	public static Collection<Etudiant> getEtudiants() {
+		return LISTE_ID_ETUDIANTS.values();
+	}
+
+	// Retourne un étudiant en fonction de son id 
+	public static Etudiant getEtudiantById(int id) {
+		return LISTE_ID_ETUDIANTS.get(id);
+	}
+
+	// Retourne le nombre d'absences d'un etudiant en fonction de son id 
+	public static Integer getAbsencesByEtudiantId(int id) {
+		return LISTE_ID_ABSENCES.get(id);
+	}
+
+}
